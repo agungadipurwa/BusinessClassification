0bbbc39e143be81afaac7fff1609a1a7287b4853
Agung Adipurwa <74081060+agungadipurwa@users.noreply.github.com>
2023-11-03 23:28:04 +0800

    fix

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from transformers import pipeline
+
+pipe = pipeline("sentiment-analysis", model="./model/")
+
+text = st.text_area('Masukan teks (contoh: Penyewaan wedding)')
+
+if text:
+    out = pipe(text)
+    print(out)
+    st.json(out)
+
